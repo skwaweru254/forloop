562429cd2e0e34ad96114d766def2d548fa873e2
skwaweru254 <skwaweru254@gmail.com>
2022-11-19 15:15:16 +0300

    range: repetition

--- forloop.py
+++ forloop.py
@@ -1,7 +1,9 @@
 print("forloop\n")
 
+#   range function
 for i in range(3):
     print("skwaweru254")
+    print("*"*(9+i))
 
 for item in range(3):
     first_name = input("Enter your first name: ")
@@ -9,4 +11,7 @@ for item in range(3):
     last_name = input("Enter your last name: ")
     print(last_name)
 
-print("Well, your names are", first_name, last_name)
+print("Well, your have done it")
+
+for i_tem in range(5):
+    print(i_tem + 1)  # autoincrement value by 1
